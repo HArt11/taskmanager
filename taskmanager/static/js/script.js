f310e244f75fc606bfe57e6923582767f06f88be
HArt11 <>
2023-08-02 08:28:40 +0000

    date picker and dropdown add to tasks page

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -3,3 +3,17 @@ document.addEventListener('DOMContentLoaded', function () {
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
 });
+
+//date picker//
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.datepicker');
+    var instances = M.Datepicker.init(elems, options);
+});
+
+// select intialisation 
+
+document.addEventListener('DOMContentLoaded', function () {
+    var selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects, options);
+});
